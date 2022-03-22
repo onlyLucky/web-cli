f2a522ecc2965b381afc5e86a8af5c1954a6c258
feynman <2901954062@qq.com>
2022-03-22 21:28:18 +0800

    vue generator init

---
+++ generator-only-vue/app/index.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: pink
+ * @Date: 2022-03-22 21:17:09
+ * @LastEditors: pink
+ * @LastEditTime: 2022-03-22 21:27:54
+ * @Description: generator-vue
+ */
+const Generator = require('yeoman-generator')
+
